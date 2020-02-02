9f7e649486087cc4b5848bcee129ccb19ecd5dec
Michael <40902786+mtrew2015@users.noreply.github.com>
2020-02-02 00:49:53 -0500

    react router dynamic routing

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+import {Routes} from './Constants/routes';
+import Landing from './Components/Landing';
+import SignUp from './Components/SignUp';
+import Login from './Components/Login'
 
 import './App.scss';
 
 function App() {
+  const COMPONENT_MAP = {
+    'Login': Login,
+    'SignUp': SignUp, 
+    'Landing': Landing 
+  }
   return (
     <div className="App">
       <h1>React Playground</h1>
+      {Routes.map(r => {
+        return <Route component={COMPONENT_MAP[r.component]} path={r.path} exact={r.exact}/>
+      })}
+      {}
     </div>
   );
 }
---
+++ playground/src/Constants/routes.js
@@ -0,0 +1,17 @@
+export const Routes = [{
+    component: "Login", 
+    path: "/login", 
+    exact:true
+}, 
+{
+    component: "Landing", 
+    path :"/", 
+    exact: true,
+},
+{
+    component: "SignUp", 
+    path:"/signup", 
+    exact: false
+},
+
+]
